0a3d4daeba2053b3d771fa6cb6734286f2bb5cbd
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-11-13 18:48:53 -0500

    impl CreateAuthor

--- x/blog/keeper/msg_server_create_author.go
+++ x/blog/keeper/msg_server_create_author.go
@@ -11,8 +11,17 @@ import (
 func (k msgServer) CreateAuthor(goCtx context.Context, msg *types.MsgCreateAuthor) (*types.MsgCreateAuthorResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	var author = types.Author{
+		Creator:   msg.Creator,
+		Firstname: msg.Firstname,
+		Lastname:  msg.Lastname,
+	}
+	id := k.AppendAuthor(
+		ctx,
+		author,
+	)
 
-	return &types.MsgCreateAuthorResponse{}, nil
+	return &types.MsgCreateAuthorResponse{
+		Id: id,
+	}, nil
 }
